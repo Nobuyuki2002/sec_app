5f046c40f25ef634d8fc81357fad3262d1eaddb7
Nobuyuki2002 <e1b20066@oit.ac.jp>
2022-10-11 14:34:49 +0900

    first commit

---
+++ src/main/java/oit/is/ouyouteam/c03/sec_app/sec_app/SecAppApplication.java
@@ -0,0 +1,13 @@
+package oit.is.ouyouteam.c03.sec_app.sec_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/ouyouteam/c03/sec_app/sec_app/SecAppApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.ouyouteam.c03.sec_app.sec_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1e18748be57b67a9fa058fe2310623c4a33822fc
Nobuyuki2002 <e1b20066@oit.ac.jp>
2022-10-11 17:11:19 +0900

    finish team task

--- src/main/java/oit/is/ouyouteam/c03/sec_app/sec_app/controller/Sample31Controller.java
+++ src/main/java/oit/is/ouyouteam/c03/sec_app/sec_app/controller/Sample31Controller.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.ouyouteam.c03.sec_app.sec_app.model.Room;
+
 @Controller
 @RequestMapping("/sample3")
 public class Sample31Controller {
@@ -46,4 +48,22 @@ public class Sample31Controller {
     return "sample37.html";
   }
 
+  @GetMapping("step8")
+  public String sample38(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
+    return "sample37.html";
+  }
+
+  @GetMapping("step9")
+  public String sample39(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    Room newRoom = new Room();
+    newRoom.addUser(loginUser);
+    model.addAttribute("new_room", newRoom);
+
+    return "sample37.html";
+  }
 }

4715006530c15d27f0ffbee52e2e8304b53621e8
GLS96mugi <kuroyuu26@gmail.com>
2022-10-11 16:02:44 +0900

    入力されたユーザ名の表示

--- src/main/java/oit/is/ouyouteam/c03/sec_app/sec_app/controller/Sample31Controller.java
+++ src/main/java/oit/is/ouyouteam/c03/sec_app/sec_app/controller/Sample31Controller.java
@@ -1,6 +1,9 @@
 package oit.is.ouyouteam.c03.sec_app.sec_app.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,4 +15,12 @@ public class Sample31Controller {
   public String sample31() {
     return "sample31.html";
   }
+
+  @GetMapping("step2")
+  public String sample32(ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
+    return "sample31.html";
+  }
+
 }
